49362e25156965c803b944391e0e96aa41255108
Agustin Zarate <agustinrafaelzarate@gmail.com>
2021-08-04 10:18:01 -0300

    Import react router dom and create route /cataloge and home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import { Cataloge } from './pages/Cataloge';
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      Hi world
-    </div>
+   <Router>
+     <Switch>
+      <Route path="/cataloge">
+        <Cataloge/>
+      </Route>
+      <Route path="/">
+        <Home/>
+      </Route>
+     </Switch>
+   </Router>
   );
 }
 
