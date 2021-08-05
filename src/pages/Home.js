751523d014ff2fc6e37430c694b597356c6771e7
Agustin Zarate <agustinrafaelzarate@gmail.com>
2021-08-05 09:54:20 -0300

    Carousel added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Carousel} from '3d-react-carousal';
+import bannerOne from '../images/banner_1.jpg';
+import bannerTwo from '../images/banner_2.jpg';
+import bannerThree from '../images/banner_3.jpg';
 
 export const Home = () => {
+    let slides = [
+        <img  src={bannerOne} alt="Banner one" />,
+        <img  src={bannerTwo} alt="2" />  ,
+        <img  src={bannerThree} alt="3" /> ];
     return(
-        <div>
-            <h1>Home</h1>
+        <div className="mt-5">
+            <Carousel slides={slides}/>
         </div>
     )
 }
