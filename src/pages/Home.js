d53fa005f0c8389a7014efb630c98a4f9c0ddafb
Agustin Zarate <agustinrafaelzarate@gmail.com>
2021-08-07 17:10:42 -0300

    import  carousel and dataAxios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,15 +3,26 @@ import {Carousel} from '3d-react-carousal';
 import bannerOne from '../images/banner_1.jpg';
 import bannerTwo from '../images/banner_2.jpg';
 import bannerThree from '../images/banner_3.jpg';
+import DataAxios from '../components/DataAxios';
+
+
 
 export const Home = () => {
+  
     let slides = [
         <img  src={bannerOne} alt="Banner one" />,
         <img  src={bannerTwo} alt="2" />  ,
         <img  src={bannerThree} alt="3" /> ];
+    
+
+
     return(
-        <div className="mt-5">
-            <Carousel slides={slides}/>
+        <div className="mb-6">
+            <div>
+                <Carousel slides={slides}/>
+            </div>
+                <DataAxios/>
+          
         </div>
     )
 }
