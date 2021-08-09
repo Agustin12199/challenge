88df4c090fa2822097d2b4e7334f04331b578f0a
Agustin Zarate <agustinrafaelzarate@gmail.com>
2021-08-09 13:43:40 -0300

    Modifying card styles

--- src/components/DataAxios.js
+++ src/components/DataAxios.js
@@ -3,16 +3,18 @@ import axios from 'axios';
 import {responseAction, errAction, loadingAction} from '../store/fetchData/action'
 import { connect } from 'react-redux';
 import { sample} from 'underscore';
+import { Link } from 'react-router-dom';
 
-const DataAxios = ({ response, err, loading, responseData}) => {
-    const url = 'https://rooftop-api-rest-frontend.herokuapp.com/items'
+
+const DataAxios = ({ response, err, loading, responseData, loadingTrue}) => {
+   
     const copyData = sample(responseData, 4) || []
-    console.log(copyData)
+
 
     useEffect(() => {
         const fetchData = () => {
             axios
-                .get(url)
+                .get('https://rooftop-api-rest-frontend.herokuapp.com/items')
                 .then((res) => {
                     response(res.data.items);
                 })
@@ -24,43 +26,47 @@ const DataAxios = ({ response, err, loading, responseData}) => {
                 });
         };
         fetchData();
+          // eslint-disable-next-line 
     }, []);
 
 
     return(
         <div className="container">
                 <section class="mb-6 portfolio-experiment  py-6 background is-flex is-justify-content-center ">
-                    <a className="py-3 px-6">
+                    <Link to="/cataloge" className="py-3 px-6">
                         <span className="text font mulish">Find the product for you</span>
                         <span className="line -right"></span>
                         <span className="line -top"></span>
                         <span className="line -left"></span>
                         <span className="line -bottom"></span>
-                    </a>
+                    </Link>
                 </section>
   
             <div className="columns is-variable is-5-desktop">
-            { copyData.map( (value) => {
+            { loadingTrue?  <div className="button is-loading is-rounded" style={{border: "none", marginTop: "150px"}}></div>  :
+             copyData.map( (value) => {
                return <div className="column">
+                        <Link to={`/cataloge/${value.id}`}>
                         <div className=" is-flex is-flex-direction-column is-align-items-center	font mulish card-container">
                             <img src={value.images[1]} alt="product" width="100%"/>
                                 <div className="is-flex is-justify-content-space-between width mt-2 px-2">
-                                    <a className="title card-title">{value.title}</a>
+                                    <p className="title card-title">{value.title}</p>
                                     <div>
-                                        <p className="font-w-6">${
-                                            value.offer? value.offer.price : value.price
-                                        }</p>
+                                        {
+                                            value.offer? 
+                                            <p className="font-w-6 green">${value.offer.price}</p>  : <p className="font-w-6 normal">${value.price}</p>
+                                        }
                                         <div class="container-price pr-2">
                                         {
                                         value.offer? <span className="offer true">${value.price}</span> : <span className="offer false"></span>
                                         }
                                         </div>
                                     </div>
-                                </div>
-                                
-                                <a href="" class="btn font-color my-5">More info</a>   
+                                </div>  
+                                <Link  to={`/cataloge/${value.id}`} class="btn font-color my-5">More info</Link>   
                         </div>
-              
+                        </Link>
+
                    </div>
             
             })}
@@ -80,7 +86,8 @@ const mapDispatchToProps = dispatch =>{
 }
 const mapStateToProps = state => {
     return{
-        responseData: state.data.response || []
+        responseData: state.data.response || [],
+        loadingTrue: state.data.loading
     }
 }
 
