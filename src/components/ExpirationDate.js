9f8ec06d6ad4c905257ac88020c02ed616fbd298
Agustin Zarate <agustinrafaelzarate@gmail.com>
2021-08-10 09:54:22 -0300

    Create my component expiration date

---
+++ src/components/ExpirationDate.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import moment from 'moment'
+
+export const ExpirationDate = ({expiration}) => {
+    
+    
+    
+    const [timer, setTimer] = useState('')
+    const [state, setState] = useState()
+  
+    
+ 
+    useEffect(() => {
+        const minute = 60;
+        const hour=60*minute;
+        const day=24*hour;
+        const offer = moment(expiration)
+        let today = moment()
+        
+        function secondsToString(seconds) {
+
+            var days = Math.floor((seconds/day)%365);
+            days = (days < 10)? '0' + days + ' day ': days + ' days ';
+
+            var hours = Math.floor((seconds/hour)%24);
+            hours = (hours < 10)? '0' + hours +  ' hour ' : hours + ' hours ';
+           
+            return days + ' : ' + hours 
+        }
+        setState( offer.diff(today, 'seconds'))
+        setTimer(secondsToString(state))
+    },[state])
+    return (
+        <div>
+            <h2>This offer expires in</h2>
+            <p>{timer}</p>
+        </div>
+    )
+}
